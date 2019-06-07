2c868dea2a7860d0b91ef763b7f5cc385fee5f83
SuchandraDatta <41965125+SuchandraDatta@users.noreply.github.com>
2019-06-07 19:08:16 +0530

    Uploading files

---
+++ LargeMovieReviewsClassification/classifyUsingBOW.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv('/gdrive/My Drive/Colab Notebooks/emotionRec/theFinalDatasetMovieReviews.csv', names=['reviews'])
+
+#Preprocess the data
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus=[]
+y=[]
+
+for i in range(0, 49968):
+ pyString=dataset['reviews'][i]
+ #print(pyString[-1])
+ y.append(np.asarray(int(pyString[-1])))
+ sentence=re.sub('[^a-zA-Z]', ' ', dataset['reviews'][i])
+ #print(sentence)
+ sentence=sentence.lower()
+ sentence=sentence.split()  
+ obj=PorterStemmer()  
+ sentence=[ obj.stem(word) for word in sentence if not word in set(stopwords.words('english')) ]
+ sentence=' '.join(sentence)
+ corpus.append(sentence)
+ 
+
+y=np.asarray(y)
+print("\nDONE PREPROCESSING")
+
+from sklearn.feature_extraction.text import CountVectorizer
+obj=CountVectorizer(max_features=1500)
+x=obj.fit_transform(corpus).toarray()
+
+print("SHPE X: ", x.shape, "\tSHPE: ", y.shape)
+print(y)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.naive_bayes import GaussianNB
+model=GaussianNB()
+model.fit(x_train, y_train)
+
+y_pred=model.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test, y_pred)
+
+print(cm)
+
+#BOW 77 percent accuracy on the test set
---
+++ LargeMovieReviewsClassification/classifyUsingWE.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv('/gdrive/My Drive/Colab Notebooks/emotionRec/theFinalDatasetMovieReviews.csv', names=['reviews'])
+x=[]
+y=[]
+for i in range(0, 49968):
+  pyString=dataset['reviews'][i]
+  y.append(np.asarray(int(pyString[-1])))
+  x.append(np.asarray(pyString))
+x=np.asarray(x)
+y=np.asarray(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2, random_state=42)
+
+from keras.preprocessing.text import Tokenizer
+tokenOBJ=Tokenizer(num_words=9000)
+tokenOBJ.fit_on_texts(x_train)
+
+x_train=tokenOBJ.texts_to_sequences(x_train)
+x_test=tokenOBJ.texts_to_sequences(x_test)
+
+voc_size=len(tokenOBJ.word_index)+1
+
+from keras.preprocessing.sequence import pad_sequences
+maxlength=100
+x_train=pad_sequences(x_train, padding='post', maxlen=maxlength)
+x_test=pad_sequences(x_test, padding='post', maxlen=maxlength)
+
+print(x_train.shape)
+print(x_test.shape)
+
+from keras.models import Sequential
+from keras import layers
+
+embedDIM=100
+
+model=Sequential()
+model.add(layers.Embedding(input_dim=voc_size, output_dim=embedDIM, input_length=maxlength))
+model.add(layers.Conv1D(64, 5, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+print(model.summary())
+history=model.fit(x_train, y_train, epochs=1, verbose=True, batch_size=4000)
+
+loss, acc1=model.evaluate(x_train, y_train)
+print("TRAIN LOSS: " , loss, "\tTRAIN ACCURACY: ", acc1*100)
+loss, acc2=model.evaluate(x_test, y_test)
+print("TEST LOSS: " , loss, "\tTEST ACCURACY: ", acc2*100)
